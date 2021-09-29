e7690bee3719883d8d74802fb33e8bdac8b52aa6
duanComputer <1530904047@qq.com>
2021-09-29 23:30:01 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="index_title">
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+body {
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,6 +17,38 @@
   text-align: center;
   color: #2c3e50;
 }
+@media (max-width: 576px) {
+  #index_title {
+    background-color: #00F;
+    height: 60px;
+  }
+}
+@media (min-width: 577px) and (max-width: 768px) {
+  #index_title {
+    height: 60px;
+    background-color: #0F0;
+  }
+}
+@media (min-width: 767px) and (max-width: 992px) {
+  #index_title {
+    height: 60px;
+    background-color: #F00;
+  }
+}
+@media (min-width: 993px) and (max-width: 1200px) {
+    #index_title {
+    height: 60px;
+      background-color: #FFF;
+  }
+}
+@media  (min-width: 1200px) {
+  #index_title {
+    height: 60px;
+    background-color: #000;
+  }
+}
+
+
 
 #nav {
   padding: 30px;
